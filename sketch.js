afe2c394c3385d466c4d24695de094875537cb14
Alison Abreu-Garcia <alison.iag@gmail.com>
2016-03-29 13:47:25 -0400

    initialize

---
+++ sketch.js
@@ -0,0 +1,24 @@
+//global variables go here
+
+
+
+//only use the preload function if you need to load images
+//remember you will need to use Anvil to run your local server
+// function preload(){
+
+// }
+
+
+//Set up your canvas
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+
+}
+
+
+//Draw your story
+function draw() {
+
+}
+
+//add your functions
